f78ff86f89f18fe7ba67bf9f58cc4930d29f1d62
amirmortezaee1 <amir.mortezaee1@gmail.com>
2023-04-04 13:03:11 +0330

    create sql schema and connect to database

---
+++ app/models.py
@@ -0,0 +1,23 @@
+import datetime  as datetime
+import sqlalchemy as sqlalchemy
+import sqlalchemy.orm as orm 
+import passlib.hash as  hash 
+
+import database as database 
+
+class UserModel(database.Base):
+    __tablemame__ = "users"
+    id = sqlalchemy.Column(sqlalchemy.Integer)
+    email = sqlalchemy.Column(sqlalchemy.String, unique= True, index=True)
+    password_hash = sqlalchemy.Column(sqlalchemy.String)
+    created_at = sqlalchemy.Column(sqlalchemy.DateTime, default = datetime.datetime.utcnow)
+    posts = orm.relationship("Post", back_populates= "users")
+
+class PostModel(database.Base):
+    __tablename__ ="posts"
+    id = sqlalchemy.Column(sqlalchemy.Integer)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("user.id"))
+    post_title = sqlalchemy.Column(sqlalchemy.String, index = True)
+    post_description = sqlalchemy.Column(sqlalchemy.String, index = True)
+    created_at = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.utcnow)
+    users = orm.relationship("User", back_populates="posts")
