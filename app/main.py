cbbd3c90086cba30b0bc59c9c5611c66ab1c559d
amirmortezaee1 <amir.mortezaee1@gmail.com>
2023-04-05 13:40:49 +0330

    create main route

--- app/main.py
+++ app/main.py
@@ -1,11 +1,5 @@
-import sqlalchemy as sqlalchemy 
-import sqlalchemy.ext.declarative as declarative
-import sqlalchemy.orm as orm 
+import fastapi as _fastapi 
+import fastapi.security as _security 
+import sqlalchemy.orm as _orm
 
-DB_URL = "sqllite:///./dbfile.db"
-
-engine =  sqlalchemy.create_engine(DB_URL, connect_args= {"check_same_thread": False})
-
-SessionLocal = orm.sessionmaker(autocommit=False, autoflush=False, bind= engine)
-
-Base = declarative.declarative_base()
+app = _fastapi.FastAPI()
