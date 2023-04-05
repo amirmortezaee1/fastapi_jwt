580344bd6dccc9c291297ecb381505f1fe5a35a3
amirmortezaee1 <amir.mortezaee1@gmail.com>
2023-04-05 23:23:03 +0330

    create schema

--- app/services.py
+++ app/services.py
@@ -3,3 +3,9 @@ import database as _database
 def create_db():
     return _database.Base.metadata.create_all(bind= _database.engine)
  
+def get_db():
+    db = _database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
